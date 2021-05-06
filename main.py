41d5f36f3a64b084a094b4761b347ebe25911c84
Vibhu Agarwal <vibhu4agarwal@gmail.com>
2021-05-06 20:29:22 +0530

    Added get_chat_history function

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
-from db.models import ChatRoom, ChatRoomInput, ChatMessage, ChatMessageInput, ObjectID
+from pymongo import ASCENDING
+
+from db.models import (
+    ChatRoom,
+    ChatRoomInput,
+    ChatMessage,
+    ObjectID,
+    json_loads,
+)
 
 app = FastAPI()
 
 
+async def get_chat_history(chat_room_id: ObjectID):
+    return json_loads(
+        list(
+            ChatMessage.objects.only("user", "time")
+            .raw({"room": chat_room_id})
+            .order_by([("time", ASCENDING)])
+            .values()
+        )
+    )
+
+
 @app.post("/chat-room/create")
-def create_chat_room(chat_input: ChatRoomInput):
+async def create_chat_room(chat_input: ChatRoomInput):
     chat_room = ChatRoom(**dict(chat_input)).save()
     chat_room_response = chat_room.to_json()
     return JSONResponse(chat_room_response, status_code=status.HTTP_201_CREATED)
 
 
 @app.patch("/chat-room/{chat_room_id}/deactivate")
-def deactivate_chat_room(chat_room_id: ObjectID):
+async def deactivate_chat_room(chat_room_id: ObjectID):
     return JSONResponse({}, status_code=status.HTTP_200_OK)
